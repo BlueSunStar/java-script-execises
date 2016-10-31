9a7804f12e11139cf26d53a9297720380851a737
MoonTide <blee.1921@gmail.com>
2016-10-30 21:06:55 -0400

    creating map-maker file. added notes to provide clarify on javascript syntax

---
+++ public_html/map-maker.js
@@ -0,0 +1,74 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * outputs a random map to the screen
+ */
+
+function createRandomBinaryArray(length){
+    // empty array (array is roughtly a list)
+    
+    var output = []; // creates an array variable in javascript
+    for (var ii = 0; ii < length; ii++){
+            output.push(Math.floor(Math.random() * 2)); //get the lowest number in a set range, then output it into an array. "push is the javascript equivalent of "add" in a java arraylist
+        }
+        return output;
+}
+
+// creates a height and weight field, then sets those proprties on the object through a setter function
+function Field (h, w) {
+    this.height = h;
+    this.width = w;
+    this.generate(); //function is decalred here, but not defined
+}
+
+
+
+Field.prototype = {
+    generate: function () { //generate function is defined here. 
+        var output = []; //creates an array variable 
+        var counter = 0;
+        while (counter < this.height){
+            output[counter] = createRandomBinaryArray(this.width); 
+            counter++;
+        }
+        this.field = output; 
+    }
+}
+
+/*
+ * In java, this would look something like this:
+ * 
+ * public class Field
+ * 
+ * private int height;
+ * private int weight;
+ * 
+ * 
+ * public int getHeight(){
+ * return height
+ * }
+ * 
+ * public int getWeight(){
+ * return weight
+ * }
+ * 
+ * public int setWeight(){
+ * return this.weight
+ * }
+ * public int setHeight()
+ * return this.height
+ * }
+ * 
+ * 
+ * public static int generateArea(){
+ * return height * weight;
+ * }
+ * 
+ */
+
+
+
